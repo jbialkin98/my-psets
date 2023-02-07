bff126ca9b0188359a40af47d9b93ddcdc3cbbd3
jbialkin98 <jbialkin98@gmail.com>
2023-02-07 15:59:49 -0500

    Add start input to int array

--- pset-1/main.c
+++ pset-1/main.c
@@ -4,22 +4,28 @@
 
 int main(int argc, char* argv[]) {
     int time_steps = atoi(argv[1]);
-    printf("%i\n", time_steps);
-
     int game_size = atoi(argv[2]);
-
     char* start = argv[3];
+    int game_array[game_size];
 
-    for (int i = 0; i <= game_size - 2; i++) {
+    printf("Initial values                  ");
+
+    // Create array of ints from start input
+    for (int i = 0; i < game_size; i++) {
+        char letter_num = start[i];
+        int num = letter_num - '0';
+        game_array[i] = num;
+    }
+    
+    for (int i = 0; i < game_size; i++) {                
         if (i == 0) {
             printf("[");
         }
-        if (i == game_size - 2) {
-            printf("%c]\n", start[i]);
+        if (i == game_size - 1) {
+            printf("%i]\n", game_array[i]);
             break;
         }
         
-        printf("%c,", start[i]);
-
+        printf("%i,", game_array[i]);
     }
 }
