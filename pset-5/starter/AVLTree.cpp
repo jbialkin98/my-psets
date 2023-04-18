d1393606b71ec50acc1976b55685578881326ad4
jbialkin98 <jbialkin98@gmail.com>
2023-04-17 23:34:35 -0400

    Finish pset

--- pset-5/starter/AVLTree.cpp
+++ pset-5/starter/AVLTree.cpp
@@ -1,5 +1,7 @@
 /*
  * Filename: AVLTree.cpp
+ * Josh Bialkin
+ * 4/15/23
  * Contains: Implementation of AVL Trees for CPSC 223
  * Part of: Homework assignment "AVL Trees" for CPSC 223
  */
@@ -50,40 +52,30 @@ static Node *new_node(int data)
 AVLTree::AVLTree()
 {
     root = nullptr;
-    // TODO: Students write code here
 }
 
 AVLTree::AVLTree(const AVLTree &source)
 {
-    // check during office hours
-
     root = pre_order_copy(source.root);
-    // TODO: Students write code here
 }
 
 AVLTree::~AVLTree()
 {
     post_order_delete(root);
-    // TODO: Students write code here
 }
 
 // assignment overload
 AVLTree &AVLTree::operator=(const AVLTree &source)
 {
+    // check for self-assignment
     if (this == &source) {
         return *this;
     }
+    // delete current tree if it exists
     post_order_delete(root);
+    // use pre-order traversal to copy the tree
     root = pre_order_copy(source.root);
     
-    // TODO: Students write code here
-    // check for self-assignment
-
-    // delete current tree if it exists
-
-    // use pre-order traversal to copy the tree
-
-    // don't forget to "return *this"
     return *this;
 }
 
@@ -136,6 +128,11 @@ void AVLTree::print_tree() const
  * BEGIN PRIVATE SECTION *
  *************************/
 
+/*
+* Input: Node root - the root of the tree
+* Returns: The leftmost node of the tree, containing the minimum value
+* Does: Traverses the tree until it reaches the leftmost node
+*/
 Node *AVLTree::find_min(Node *node) const
 {
     assert(node != nullptr);
@@ -143,13 +140,13 @@ Node *AVLTree::find_min(Node *node) const
         node = node->left;
     }
     return node;
-    // TODO: Students write code here
-
-    // This line is in here so that the starter code compiles.
-    // Remove or modify it when implementing.
-    
 }
 
+/*
+* Input: Node root - the root of the tree
+* Returns: The rightmost node of the tree, containing the maximum value
+* Does: Traverses the tree until it reaches the rightmost node
+*/
 Node *AVLTree::find_max(Node *node) const
 {
     assert(node != nullptr);
@@ -157,28 +154,28 @@ Node *AVLTree::find_max(Node *node) const
         node = node->right;
     }
     return node;
-    // TODO: Students write code here
-
-    // This line is in here so that the starter code compiles.
-    // Remove or modify it when implementing.
 }
 
+/*
+* Input: Node root - the root of the tree
+* Returns: False if the tree does not contain the node, true if it does
+* Does: Traverses the tree until the value is found or the end of the tree is 
+        reached. The function is recursively called on each node, the node to 
+        the left or the right of the current node depending on whether the 
+        value it is searching for is less than or greater than the current 
+        node.
+*/
 bool AVLTree::contains(Node *node, int value) const
 {
     if (node == nullptr) {
         return false;
     }
     if (node->data > value) {
-        contains(node->left, value);
+        return contains(node->left, value);
     } else if (node->data < value) {
-        contains(node->right, value);
-    } else {
-        return true;
+        return contains(node->right, value);
     }
-    // TODO: Students write code here
-
-    // This line is in here so that the starter code compiles.
-    // Remove or modify it when implementing.
+    return true;
 }
 
 Node *AVLTree::insert(Node *node, int value)
@@ -278,19 +275,26 @@ Node *AVLTree::remove(Node *node, int value)
     return root;
 }
 
+/*
+* Input: Node
+* Returns: The height of the node called, or -1 if the node is null
+* Does: Checks whether the node called is null, and returns -1 if so. If not,
+        returns the height of the node.
+*/
 int AVLTree::tree_height(Node *node) const
 {
-    // check during office hours
-
-    // TODO: Students write code here
-    if (node == NULL){
+    if (node == NULL) {
         return -1;
     }
-    // This line is in here so that the starter code compiles.
-    // Remove or modify it when implementing.
     return node->height;
 }
 
+/*
+* Input: Node
+* Returns: The number of nodes in the tree
+* Does: Recursively calls the function using pre-order traversal to count
+        the number of nodes in the tree.
+*/
 int AVLTree::node_count(Node *node) const
 {
     if (node == nullptr) {
@@ -299,28 +303,32 @@ int AVLTree::node_count(Node *node) const
 
     int left_nodes = node_count(node->left);
     int right_nodes = node_count(node->right);
-    // TODO: Students write code here
 
-    // This line is in here so that the starter code compiles.
-    // Remove or modify it when implementing.
     return 1 + left_nodes + right_nodes;
 }
 
+/*
+* Input: Node
+* Returns: The numbers of the tree added up including multiplicity of nodes
+* Does: Recursively calls the function using pre-order traversal, adding the
+        multiplicity of each node multipied by its value to get the total
+        sum of every node in the tree.
+*/
 int AVLTree::count_total(Node *node) const
 {
-    // check during office hours
     if (node == NULL) {
         return 0;
     }
-
-    int node_multiplicity = node->count * node->data;
-    // TODO: Students write code here
-
-    // This line is in here so that the starter code compiles.
-    // Remove or modify it when implementing.
-    return (node->count * node->data) + count_total(node->left) + count_total(node->right);
+    return (node->count * node->data) + count_total(node->left) + 
+        count_total(node->right);
 }
 
+/*
+* Input: Node node - a pointer to the root of the tree to copy
+* Returns: a deep copy of the tree rooted at node
+* Does: Performs a pre-order traversal of the tree rooted at node to
+*      create a deep copy of it
+*/
 Node *AVLTree::pre_order_copy(Node *node) const
 {
     if (node == NULL)
@@ -332,13 +340,16 @@ Node *AVLTree::pre_order_copy(Node *node) const
 
     new_node->left = pre_order_copy(node->left);
     new_node->right = pre_order_copy(node->right);
-    // TODO: Students write code here
-    // (hint: use a pre-order traversal to copy details from the
-    // node to a new_node)
 
     return new_node;
 }
 
+/*
+* Input: Node node - a pointer to the root of the tree to delete
+* Returns: N/A
+* Does: Performs a post-order traversal of the tree rooted at node to
+*      delete every node in that tree
+*/
 void AVLTree::post_order_delete(Node *node)
 {
     if (node == NULL) {
@@ -347,56 +358,87 @@ void AVLTree::post_order_delete(Node *node)
     post_order_delete(node->left);
     post_order_delete(node->right);
     delete node;
-    // TODO: Students write code here
 }
 
+/*
+* Input: Node root - a node of the avl tree.
+* Returns: the balanced subtree.
+* Does: If unbalanced, balances the tree rooted at node.
+*/
 Node *AVLTree::balance(Node *node)
 {
     int diff_in_height = height_diff(node);
-    // TODO: Students write code here
 
-    // This line is in here so that the starter code compiles.
-    // Remove or modify it when implementing.
+    if (diff_in_height > 1 && tree_height(node->left->left) > 
+        tree_height(node->left->right)) {
+            return right_rotate(node);
+    } else if (diff_in_height < -1 && tree_height(node->right->right) > 
+        tree_height(node->right->left)) {
+            return left_rotate(node);
+    } else if (diff_in_height > 1) {
+        node->left = left_rotate(node->left);
+        return right_rotate(node);
+    } else if (diff_in_height < -1) {
+        node->right = right_rotate(node->right);
+        return left_rotate(node);
+    }
+    
     return node;
 }
 
+/*
+* Input: Node node - a node of the avl tree.
+* Returns: pointer to the root of rotated tree.
+* Does: right rotate tree rooted at node
+*/
 Node *AVLTree::right_rotate(Node *node)
 {
-    // TODO: Students write code here
+    Node *new_root = node->left;
+    Node *new_left = new_root->right;
+    new_root->right = node;
+    node->left = new_left;
 
-    // This line is in here so that the starter code compiles.
-    // Remove or modify it when implementing.
-    return node;
+    node->height = 1 + max(tree_height(node->left), tree_height(node->right));
+    new_root->height = 1 + max(tree_height(new_root->left), tree_height(new_root->right));
+
+    return new_root;
 }
 
+/*
+* Input: Node node - a node of the avl tree.
+* Returns: pointer to the root of rotated tree.
+* Does: left rotate tree rooted at node
+*/
 Node *AVLTree::left_rotate(Node *node)
 {
-    // TODO: Students write code here
+    Node *new_root = node->right;
+    Node *new_right = new_root->left;
+    new_root->left = node;
+    node->right = new_right;
 
-    // This line is in here so that the starter code compiles.
-    // Remove or modify it when implementing.
-    return node;
+    node->height = 1 + max(tree_height(node->left), tree_height(node->right));
+    new_root->height = 1 + max(tree_height(new_root->left), 
+        tree_height(new_root->right));
+    
+    return new_root;
 }
 
+/*
+* Input: Node node - a node of the avl tree.
+* Returns: integer value signifying the height difference.
+* Does: calculates the difference in the height of the left and child
+*       subtree of node
+*/
 int AVLTree::height_diff(Node *node) const
-{
-    // call tree height, don't return absolute value
-    
-    assert(node != nullptr);
-    int left_node_height = -1;
-    int right_node_height = -1;
-    if (node->left != nullptr) {
-        left_node_height = node->left->height;
+{    
+    if (node == NULL) {
+        return 0;
     }
-    if (node->right != nullptr) {
-        right_node_height = node->right->height;
-    }
-    int difference = abs(left_node_height-right_node_height);
-    // TODO: Students write code here
 
-    // This line is in here so that the starter code compiles.
-    // Remove or modify it when implementing.
-    return difference;
+    int left_node_height = tree_height(node->left);
+    int right_node_height = tree_height(node->right);
+
+    return left_node_height - right_node_height;
 }
 
 Node *AVLTree::find_parent(Node *node, Node *child) const
--- pset-5/starter/LOG.md
+++ pset-5/starter/LOG.md
@@ -11,14 +11,15 @@ jsb245
 | 4/15 |       9:00pm |       2:00 | implement functions in order until tree height |
 | 4/16 |      11:30am |       0:30 | work on height and node counting functions |
 | 4/16 |       1:10pm |       0:50 | fix errors with height and counting nodes |
+| 4/16 |       7:30pm |       1:20 | implement avl balancing, having an error with double rotations |
+| 4/17 |       9:30pm |       1:00 | fix double rotate error in right rotate function, conform code to style guide |
 
-
-Total time: 
+Total time: 5:40
 
 # Collaboration
 N/A
 
 # Discussion
-I didn't have that much trouble with this pset, but I did have a hard time debugging a little issue. I was using print statements to find where my error was occuring, but I didn't realize that C++ sometimes needed an "endl" after the print statement to correcty print to the terminal. Once I realized this, I could tell what functions were being called, where the program was failing, and how to narrow down the error.
+It took me a bit of time to understand how to translate my conceptual understanding of AVL trees to coding them, specifically in the balance and rotation functions. I was able to get close, but there was often one line of my implementation that was incorrect and causing errors, which I found difficult to find. I do feel that I now have a better idea of the implementation and was able to fix the errors.
 
-I felt that most of this translated easily from C, so I wasn't too lost. The other issue I had was in running the testing scripts. I got them all to pass after attending office hours, but my code initially produced the correct output whle failing a few of the test cases. I found reading these test cases to be difficult, but some of the ULA's were able to help me identify the functions that were causing the tests to fail, and I could solve it from there.
+My biggest conceptual error was figuring out the algorithm of when to call what rotate function. I was confident in my height_diff function, but figuring out what exactly to do with that information took some time, particularly when it came to the double rotations. 
